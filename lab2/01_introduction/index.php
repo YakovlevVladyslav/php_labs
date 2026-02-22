53f16d575ae6c4bfed7f04ffd3d247e4033e7cd4
YakovlevVladyslav <yakovlevvladyslav6@gmail.com>
2026-02-22 22:27:54 +0200

    second laboratory work

---
+++ lab2/01_introduction/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Привет, мир!<br />";
+echo "Hello, World with echo!<br />";
+print "Hello, World with print!<br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo $days.$message."<br />";
+echo "{$days}{$message}<br />";
